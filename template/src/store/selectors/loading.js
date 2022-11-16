5ca5e40437b91a93d70d63ed49a6e3f90082a824
vunguyendhSTS <vu.nguyendh@saigontechnology.com>
2022-11-16 13:36:14 +0700

    remove unnecessary comment

--- template/src/store/selectors/loading.js
+++ template/src/store/selectors/loading.js
@@ -1,6 +1,5 @@
 export const getLoadingSelector = (state, actionTypes) => {
   if (Array.isArray(actionTypes)) {
-    // some of element of list actionTypes is dispatched in redux will return state loading
     return actionTypes.some(r => {
       const matches = /(.*)_(REQUEST|SUCCESS|FAILURE)/.exec(r)
       if (!matches) {
