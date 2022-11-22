83adc98fe300a1c919a1e1356d18256486c355c9
vunguyendhSTS <117509324+vunguyendhSTS@users.noreply.github.com>
2022-11-22 11:50:17 +0700

    common type creator and loading reducer state (#6)
    * common creator and loading reducer
    
    * update wrong type for success and failure
    
    * remove unnecessary comment

--- template/src/store/reducers/user.js
+++ template/src/store/reducers/user.js
@@ -1,4 +1,6 @@
 import {createSlice} from '@reduxjs/toolkit'
+import {APP_CONSTANTS_ACTIONS} from '../constants/app'
+import {USER_CONSTANTS_ACTIONS} from '../constants/user'
 
 export const initialState = {
   userInfo: {},
@@ -11,6 +13,18 @@ export const userSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
+    [USER_CONSTANTS_ACTIONS.USER_LOGIN_ACTIONS.HANDLER]: () => {},
+    [USER_CONSTANTS_ACTIONS.USER_LOGIN_ACTIONS.SUCCESS]: () => {},
+    [USER_CONSTANTS_ACTIONS.USER_LOGIN_ACTIONS.FAILURE]: () => {},
+    [USER_CONSTANTS_ACTIONS.USER_SIGN_UP_ACTIONS.HANDLER]: () => {},
+    [USER_CONSTANTS_ACTIONS.USER_SIGN_UP_ACTIONS.SUCCESS]: () => {},
+    [USER_CONSTANTS_ACTIONS.USER_SIGN_UP_ACTIONS.FAILURE]: () => {},
+    [USER_CONSTANTS_ACTIONS.USER_LOG_OUT_ACTIONS.HANDLER]: () => {},
+    [USER_CONSTANTS_ACTIONS.USER_LOG_OUT_ACTIONS.SUCCESS]: () => {},
+    [USER_CONSTANTS_ACTIONS.USER_LOG_OUT_ACTIONS.FAILURE]: () => {},
+    [USER_CONSTANTS_ACTIONS.UPDATE_USER_INFO_ACTIONS.HANDLER]: () => {},
+    [USER_CONSTANTS_ACTIONS.UPDATE_USER_INFO_ACTIONS.SUCCESS]: () => {},
+    [USER_CONSTANTS_ACTIONS.UPDATE_USER_INFO_ACTIONS.FAILURE]: () => {},
     userLogin: () => {},
     userSignUp: () => {},
     userLoginSuccess: (state, action) => {},
@@ -20,5 +34,20 @@ export const userSlice = createSlice({
   extraReducers: builder => {},
 })
 
-export const userActions = userSlice.actions
+export const userActions = {
+  ...userSlice.actions,
+  userLoginHandle: userSlice.actions[USER_CONSTANTS_ACTIONS.USER_LOGIN_ACTIONS.HANDLER],
+  userLoginSuccess: userSlice.actions[USER_CONSTANTS_ACTIONS.USER_LOGIN_ACTIONS.SUCCESS],
+  userLoginFailure: userSlice.actions[USER_CONSTANTS_ACTIONS.USER_LOGIN_ACTIONS.FAILURE],
+  userSignUpHandle: userSlice.actions[USER_CONSTANTS_ACTIONS.USER_SIGN_UP_ACTIONS.HANDLER],
+  userSignUpSuccess: userSlice.actions[USER_CONSTANTS_ACTIONS.USER_SIGN_UP_ACTIONS.SUCCESS],
+  userSignUpFailure: userSlice.actions[USER_CONSTANTS_ACTIONS.USER_SIGN_UP_ACTIONS.FAILURE],
+  userLogoutHandle: userSlice.actions[USER_CONSTANTS_ACTIONS.USER_LOG_OUT_ACTIONS.HANDLER],
+  userLogoutSuccess: userSlice.actions[USER_CONSTANTS_ACTIONS.USER_LOG_OUT_ACTIONS.SUCCESS],
+  userLogoutFailure: userSlice.actions[USER_CONSTANTS_ACTIONS.USER_LOG_OUT_ACTIONS.FAILURE],
+  updateUserInfoHandle: userSlice.actions[USER_CONSTANTS_ACTIONS.UPDATE_USER_INFO_ACTIONS.HANDLER],
+  updateUserInfoSuccess: userSlice.actions[USER_CONSTANTS_ACTIONS.UPDATE_USER_INFO_ACTIONS.SUCCESS],
+  updateUserInfoFailure: userSlice.actions[USER_CONSTANTS_ACTIONS.UPDATE_USER_INFO_ACTIONS.FAILURE],
+}
+
 export default userSlice.reducer
---
+++ template/src/store/selectors/loading.js
@@ -0,0 +1,13 @@
+export const getLoadingSelector = (state, actionTypes) => {
+  if (Array.isArray(actionTypes)) {
+    return actionTypes.some(r => {
+      const matches = /(.*)_(REQUEST|SUCCESS|FAILURE)/.exec(r)
+      if (!matches) {
+        return false
+      }
+      const [, requestName, requestState] = matches
+      return state.loading[`${requestName}`] || false
+    })
+  }
+  return false
+}
