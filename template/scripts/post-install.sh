51bc39b2ef0c79c81de24421c29a6da1f67b9fab
loidev <nhoxbaycao@gmail.com>
2022-12-29 15:58:57 +0700

    Revert "remove jetify and add npx"
    This reverts commit 5d244db4b5a8b9c2c7df384b086a7674cb0b1745.

--- template/scripts/post-install.sh
+++ template/scripts/post-install.sh
@@ -1,5 +1,7 @@
 #1/use/bin/env bash
 
+yarn jetify
+
 if [[ ! "$CI" = true ]]; then
   # Do not install iOS Pods on CI
   if [[ $(sysctl -n machdep.cpu.brand_string) =~ "Apple" ]]; then
@@ -7,7 +9,7 @@ if [[ ! "$CI" = true ]]; then
     cd ./ios && arch -x86_64 pod install && cd ..
   else
     echo "Intel"
-    cd ./ios && npx pod install && cd ..
+    cd ./ios && pod install && cd ..
   fi
   error_code=$?
   echo "Pods error_code: ${error_code}"
@@ -19,7 +21,7 @@ if [[ ! "$CI" = true ]]; then
         arch -x86_64 pod install --repo-update && cd ..
     else
       echo "Intel"
-      npx pod install --repo-update && cd ..
+      pod install --repo-update && cd ..
     fi
   else
     exit $error_code
