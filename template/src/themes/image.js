fe8aa663887d9858410ffe1683550eba303b6d9a
Anh Le <anh.lehoang@saigontechnology.com>
2023-01-17 10:33:24 +0700

    chore: update generate image script

--- template/src/themes/image.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable global-require */
-/**
- * @flow
- */
-
-const Images = {
-  sts: require('./../assets/images/sts.png'),
-}
-export default Images
--- template/src/themes/images.js
+++ template/src/themes/images.js
@@ -1,1 +1,9 @@
-export const images = {}
+/* eslint-disable global-require */
+/**
+ * @flow
+ */
+
+const Images = {
+  sts: require('./../assets/images/sts.png'),
+}
+export default Images
