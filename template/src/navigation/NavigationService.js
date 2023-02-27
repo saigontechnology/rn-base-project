4733e7476fff2dff0ff53a216a6aedff296bc1d7
ngochuyduong <huy.duong@saigontechnology.com>
2023-02-27 13:31:50 +0700

    chore: add some navigation functions

--- template/src/navigation/NavigationService.js
+++ template/src/navigation/NavigationService.js
@@ -1,5 +1,5 @@
 import {StackActions} from '@react-navigation/native'
-import {createNavigationContainerRef} from '@react-navigation/native';
+import {createNavigationContainerRef} from '@react-navigation/native'
 
 export const navigationRef = createNavigationContainerRef()
 
@@ -21,4 +21,59 @@ export function popToTop() {
   if (navigationRef.isReady()) {
     navigationRef.dispatch(StackActions.popToTop())
   }
+}
+
+export const canGoBack = () => navigationRef.isReady() && navigationRef.canGoBack()
+
+export const getState = () => navigationRef.isReady() && navigationRef.getState()
+
+export const getParent = () => navigationRef.isReady() && navigationRef.getParent()
+
+export const push = (name, params) => {
+  if (navigationRef.isReady()) {
+    navigationRef.dispatch(StackActions.push(name, params))
+  }
+}
+
+export const replace = (name, params) => {
+  if (navigationRef.isReady()) {
+    navigationRef.dispatch(StackActions.replace(name, params))
+  }
+}
+
+export const reset = state => {
+  if (navigationRef.isReady()) {
+    navigationRef.reset(state)
+  }
+}
+
+export const resetTo = name => {
+  if (navigationRef.isReady()) {
+    navigationRef.reset({
+      index: 0,
+      routes: [{name}],
+    })
+  }
+}
+
+export const goBack = () => {
+  if (canGoBack()) {
+    navigationRef.goBack()
+  }
+}
+
+export const setParams = params => {
+  if (navigationRef.isReady()) {
+    navigationRef.setParams(params)
+  }
+}
+
+export const dispatch = action => {
+  if (navigationRef.isReady()) {
+    navigationRef.dispatch(action)
+  }
+}
+
+export const getCurrentRouteName = () => {
+  return navigationRef.isReady() ? navigationRef?.getCurrentRoute()?.name : ''
 }
